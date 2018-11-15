8c787f68603933b687ba6dab7efdc7465871b9b0
DiegoSA <diego.sa.pb@gmail.com>
2018-11-15 01:40:47 -0200

    create some methods at main screen

--- app/src/main/java/multipel/justificarvisita/screens/JustifyScreen.java
+++ app/src/main/java/multipel/justificarvisita/screens/JustifyScreen.java
@@ -2,13 +2,29 @@ package multipel.justificarvisita.screens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import multipel.justificarvisita.R;
 
 public class JustifyScreen extends AppCompatActivity {
+
+    private Button newClient;
+    private Button saveJustify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_justify_screen);
+
+        newClient = (Button) findViewById(R.id.buttonNewClient);
+        saveJustify = (Button) findViewById(R.id.buttonSave);
+
+        newClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
