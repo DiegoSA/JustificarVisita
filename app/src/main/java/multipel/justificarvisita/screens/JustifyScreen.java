652f4f322bebcbea11796a1aa35c4232ed33d09f
DiegoSA <diego.sa.pb@gmail.com>
2018-11-20 03:18:34 -0200

    I started to put some elements to work

--- app/src/main/java/multipel/justificarvisita/screens/JustifyScreen.java
+++ app/src/main/java/multipel/justificarvisita/screens/JustifyScreen.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import multipel.justificarvisita.R;
 
@@ -11,6 +15,10 @@ public class JustifyScreen extends AppCompatActivity {
 
     private Button newClient;
     private Button saveJustify;
+    private SimpleDateFormat sd;
+    private Date date;
+    private TextView textViewToday;
+    private TextView textViewRcaName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,13 @@ public class JustifyScreen extends AppCompatActivity {
 
         newClient = (Button) findViewById(R.id.buttonNewClient);
         saveJustify = (Button) findViewById(R.id.buttonSave);
+        textViewToday = (TextView) findViewById(R.id.textViewToday);
+        textViewRcaName = (TextView) findViewById(R.id.textViewRcaName);
+        sd = new SimpleDateFormat("dd-MM-yyyy");
+        date = new Date();
+        String today = sd.format(date);
+        textViewToday.setText(today);
+        textViewRcaName.setText("Diego");
 
         newClient.setOnClickListener(new View.OnClickListener() {
             @Override
